9e9e79540bb9ba744f551f1664b8f8df2ba0cf2d
800006913 <yvdantas@banestes.com.br>
2025-02-17 20:50:45 -0300

    section 24: preparing the app for state management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from './NewPost';
 import Post from './Post'
 import classes from './PostsList.module.css'
 
 function PostsList() {
-    return (<ul className={classes.posts}>
+    
+    return <>
+    <NewPost/>
+    <ul className={classes.posts}>
             <Post author="Yuri" body="Amazing!" />
             <Post author="Max" body="lol!!" />
     </ul>
-    );
+    </>
 }
 
 export default PostsList;
