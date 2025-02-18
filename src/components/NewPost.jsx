bff21b52186d65fdd9c385b02a09ef8ac59756fe
800006913 <yvdantas@banestes.com.br>
2025-02-17 21:09:13 -0300

    section 26: working with state

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
 function NewPost() {
+  const [enteredBody, setEnteredBody] = useState('');
   function changeBodyHandler(e) {
-    console.log(e.target.value);
+    setEnteredBody(e.target.value);
   }
 
   return (
@@ -11,6 +13,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
