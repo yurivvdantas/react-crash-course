a13b164c6127bdd5f8981ef1a424658b7abdfe14
800006913 <yurivvdantas@gmail.com>
2025-02-20 20:46:45 -0300

    chore: prettie configuration

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -5,11 +5,11 @@ function NewPost(props) {
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
+        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange}/>
+        <input type="text" id="name" required onChange={props.onAuthorChange} />
       </p>
     </form>
   );
