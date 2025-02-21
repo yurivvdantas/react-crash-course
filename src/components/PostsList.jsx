e651a7319b153ee14730fcc763b9390d4b5babc5
800006913 <yurivvdantas@gmail.com>
2025-02-21 20:08:39 -0300

    section 28: the special children prop

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,28 +1,33 @@
+import Modal from './Modal';
 import NewPost from './NewPost';
-import Post from './Post'
-import classes from './PostsList.module.css'
+import Post from './Post';
+import classes from './PostsList.module.css';
 import { useState } from 'react';
 
 function PostsList() {
-    const [enteredBody, setEnteredBody] = useState('');
-    function bodyChangeHandler(event) {
+  const [enteredBody, setEnteredBody] = useState('');
+  function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
-    }
-    const [enteredAuthor, setEnteredAuthor] = useState('');
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
-    
-    return (
+  }
+  const [enteredAuthor, setEnteredAuthor] = useState('');
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
     <>
-    <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}
-/>
-    <ul className={classes.posts}>
-            <Post author={enteredAuthor} body={enteredBody} />
-            <Post author="Yuri" body="Lol" />
-    </ul>
-    </>    
-    );
+      <Modal>
+        <NewPost
+          onBodyChange={bodyChangeHandler}
+          onAuthorChange={authorChangeHandler}
+        />
+      </Modal>
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Yuri" body="Lol" />
+      </ul>
+    </>
+  );
 }
 
 export default PostsList;
