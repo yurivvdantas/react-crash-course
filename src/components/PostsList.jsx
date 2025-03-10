67d7288be604c3baca39fc59ea48e9bece548052
Yuri Vinturini <yurivvdantas@gmail.com>
2025-03-10 09:02:19 -0300

    section 31: adding form buttons

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -21,6 +21,7 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost
             onBodyChange={bodyChangeHandler}
             onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
           />
         </Modal>
       )}
