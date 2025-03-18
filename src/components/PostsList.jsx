4b13cd25cb63e93ac887e095ab86c71fd5cf111f
Yuri Vinturini <yurivvdantas@gmail.com>
2025-03-18 08:53:30 -0300

    section 34:  outputting List Data

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -16,9 +16,19 @@ function PostsList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostsHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author="Yuri" body="Lol" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
