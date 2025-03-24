a7e39cc416b7dfcb33cec1b763d885d6db6eff49
Yuri Vinturini <yurivvdantas@gmail.com>
2025-03-24 08:48:43 -0300

    section 36: sending a post http request

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -7,6 +7,14 @@ import classes from './PostsList.module.css';
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
   function addPostsHandler(postData) {
+    fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
