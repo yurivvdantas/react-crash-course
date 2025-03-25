2e15c4e31a29dd87735f62333287d14dc2c4b6b0
800006913 <yurivvdantas@gmail.com>
2025-03-25 08:30:32 -0300

    section 37: handling side effects with useEffect()

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Modal from './Modal';
 import NewPost from './NewPost';
 import Post from './Post';
@@ -6,6 +6,16 @@ import classes from './PostsList.module.css';
 
 function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch('http://localhost:8080/posts');
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+
+    fetchPosts();
+  }, []);
+
   function addPostsHandler(postData) {
     fetch('http://localhost:8080/posts', {
       method: 'POST',
