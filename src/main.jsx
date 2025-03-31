b11d073be7eea25c008e3cd60e91d93af4d13b6d
800006913 <yurivvdantas@gmail.com>
2025-03-31 18:17:56 -0300

    section 40: adding routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NewPost from './components/NewPost';
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: 'create-post', element: <NewPost /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
